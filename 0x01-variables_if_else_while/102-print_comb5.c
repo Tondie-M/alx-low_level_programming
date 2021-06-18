4ccdcd788d41b4511003ce4e759ba18660cf6b5d
Tondie-M <teeteem1023@gmail.com>
2021-06-18 10:33:27 -0700

    Two digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  *
  */
-
 int main(void)
 {
 	int fd, sd, td = 48;
@@ -14,47 +12,33 @@ int main(void)
 
 	while (fd < 48)
 	{
-		putchar(fd); putchar(sd); putchar(td); putchar(ld);
-
+		putchar(fd);
+		putchar(sd);
+		putchar(td);
+		putchar(ld);
 		if (fd == 57 && sd == 56 && td == 57 && ld == 57)
-		{
-			putchar('\n');
-			fd++;
-		}
+		{ putchar('\n');
+			fd++; }
 		else
-		{
-			putchar(44);
+		{ putchar(44);
 			putchar(32);
 			if (td == 57 && ld == 57)
 			{
 				if (sd < 56)
-				{
-					ld = sd++ + 1;
-					td = fd;
-				}
+				{ ld = sd++ + 1;
+					td = fd; }
 				else if (sd == 56)
 				{
 					sd++;
 					td = fd + 1;
-					ld = 48;
-				}
+					ld = 48; }
 				else if (sd == 57)
-				{
-					sd = 48;
+				{ sd = 48;
 					ld = 49;
-					td = ++fd;
-				}
-			}
+					td = ++fd; }}
 			else if (ld < 57)
-			{
-				ld++;
-			}
+			{ ld++; }
 			else
-			{
-				ld = 48;
-				td++;
-			}
-		}
-	}
-	return (0);
-}
+			{ ld = 48;
+				td++; }}}
+return (0); }
