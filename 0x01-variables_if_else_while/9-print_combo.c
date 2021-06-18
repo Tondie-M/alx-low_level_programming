e8a49fc91f03a1906bae0a9d57581da5290f1166
Tondie-M <teeteem1023@gmail.com>
2021-06-18 06:54:50 -0700

    Single digits

---
+++ 0x01-variables_if_else_while/9-print_combo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 48;
+
+	while (number < 58)
+	{
+		putchar(number);
+		if (number != 57)
+		{
+			putchar(44);
+			putchar(35);
+			number++;
+		}
+		else
+		{
+			number++;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
