9e5c2edcb7df00adb25f9749da448e2c3efd5317
Tondie-M <teeteem1023@gmail.com>
2021-06-18 10:48:33 -0700

    Two digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,15 +17,18 @@ int main(void)
 		putchar(td);
 		putchar(ld);
 		if (fd == 57 && sd == 56 && td == 57 && ld == 57)
-		{ putchar('\n');
+		{
+			putchar('\n');
 			fd++; }
 		else
-		{ putchar(44);
+		{
+			putchar(44);
 			putchar(32);
 			if (td == 57 && ld == 57)
 			{
 				if (sd < 56)
-				{ ld = sd++ + 1;
+				{
+					ld = sd++ + 1;
 					td = fd; }
 				else if (sd == 56)
 				{
@@ -33,7 +36,8 @@ int main(void)
 					td = fd + 1;
 					ld = 48; }
 				else if (sd == 57)
-				{ sd = 48;
+				{
+					sd = 48;
 					ld = 49;
 					td = ++fd; }}
 			else if (ld < 57)
