566a123c40f56c20f90c640d84a5dd16eddc07c9
Tondie-M <teeteem1023@gmail.com>
2021-06-22 11:49:32 -0700

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,7 +9,16 @@
 
 int main(void)
 {
-	string h = "Holberton";
-	_putchar("Holberton\n");
+	int i = 0;
+	char c;
+	char cs[] = "Holberton\n"
+
+	while (i < 9)
+	{
+		c = cs[i];
+
+		_putchar(c);
+		i++;
+	}
 	return (0);
 }
