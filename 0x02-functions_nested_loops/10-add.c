87693955f5765f27e2d7278911f8b11011ceb14f
Tondie-M <teeteem1023@gmail.com>
2021-06-22 11:21:20 -0700

    Sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - Adss two numbers
+ * @i: first number
+ * @j: second number
+ *
+ * Return: sum of i and j
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
