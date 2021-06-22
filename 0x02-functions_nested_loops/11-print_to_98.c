63794a01c256910396a6042865c2d59e225e9cbc
Tondie-M <teeteem1023@gmail.com>
2021-06-22 11:34:40 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from value n to 98
+ * @i: input value
+ *
+ * Return: void
+ */
+
+void print_to_98(int i)
+{
+	while (n != 98)
+	{
+		printf("%d, ", i);
+		if (i > 98)
+		{
+			i--;
+		}
+		else
+		{
+			i++;
+		}
+	}
+}
