0cfee155eb84d52b6bbbf294d7ee43cdc1a1accb
Tondie-M <teeteem1023@gmail.com>
2021-06-24 03:46:00 -0700

    product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Get the product of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
