163b8a5791710d30ac076e84220daf678d6da755
Tondie-M <teeteem1023@gmail.com>
2021-06-24 11:30:45 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,25 +17,18 @@ int main(void)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else if (i % 15 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else
+		if (i % 3 != 0 && i % 5 != 0)
 		{
 			printf("%d", i);
 		}
-
-		if (i < 100)
-		{
+		if (i != 100)
 			printf(" ");
-		}
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
 
