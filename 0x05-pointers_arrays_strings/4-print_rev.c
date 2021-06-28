e2bf511f6ec86ba74eff8f96b12df92bacd8b367
Tondie-M <teeteem1023@gmail.com>
2021-06-28 08:03:31 -0700

    Revese

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_rev  -  prints a string, in reverse, followed by a new line
+ * @s: dtring to reverse
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int index = 0;
+
+	while (*(s + index))
+		index++;
+	index = index - 1;
+	while (index >=0)
+	{
+		_putchar(*(s + index));
+		index--;
+	}
+	_putchar('\n');
+}
