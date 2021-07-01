a171b4415025ca765e0afbfc593586b7c55440ec
Tondie-M <teeteem1023@gmail.com>
2021-07-01 06:20:48 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: pointer to the resulting string 
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+
+
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+		while (src[i] != '\0')
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
