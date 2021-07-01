9128cabf63969950420f63aaaf2d35b070f8854e
Tondie-M <teeteem1023@gmail.com>
2021-07-01 07:41:31 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: points to the destinantion string
+ * @src: points to the source string
+ * @n: int
+ *
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i++] = '\0';
+	}
+
+	return (dest);
+}
