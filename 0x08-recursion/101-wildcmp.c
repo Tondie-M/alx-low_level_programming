cad20ef96fc8892597c4ced147208bfcd965fa96
Tondie-M <teeteem1023@gmail.com>
2021-07-07 08:57:53 -0700

    compare

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings
+ * @s1: String one
+ * @s2: String two
+ *
+ * Return: int
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+	{
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+		else if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	}
+
+	return (0);
+}
