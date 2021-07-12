e278096a707a3ddf6850bdb923fa2f2e463fba67
Tondie-M <teeteem1023@gmail.com>
2021-07-12 08:07:35 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: Pointer
  *
- * Retun: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
