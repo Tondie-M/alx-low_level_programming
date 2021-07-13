afb531e84fb871503e790da9db278a9cad4489df
Tondie-M <teeteem1023@gmail.com>
2021-07-13 08:42:24 -0700

    frees a 2 dimensional grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * free_grid -  frees a 2 dimensional grid
+ * @grid: String
+ * @height: Height
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
